7dd5bc8590d5408bf9186cf648099d21807d477c
Vaddiaditya75 <vaddiaditya75@gmail.com>
2022-05-26 20:05:08 +0530

    EMP2 Changes

--- src/org/git/Sample.java
+++ src/org/git/Sample.java
@@ -3,6 +3,7 @@ package org.git;
 public class Sample {
 	public static void main(String[] args) {
 		System.out.println("Git hub intro");
+		System.out.println("EMP1 changes");
 		System.out.println("EMP2 changes");
 	}
 
